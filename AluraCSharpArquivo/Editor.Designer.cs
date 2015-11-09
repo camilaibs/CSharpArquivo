f19b8012cf894706aa15ae8d68468a355bc21a7b
Camila Silva <camilaibs@gmail.com>
2015-11-08 23:42:06 -0200

    Projeto

---
+++ AluraCSharpArquivo/Editor.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AluraCSharpArquivo
+{
+    partial class Editor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.texto = new System.Windows.Forms.TextBox();
+            this.botaoSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // texto
+            // 
+            this.texto.Location = new System.Drawing.Point(13, 24);
+            this.texto.Multiline = true;
+            this.texto.Name = "texto";
+            this.texto.Size = new System.Drawing.Size(267, 190);
+            this.texto.TabIndex = 0;
+            // 
+            // botaoSalvar
+            // 
+            this.botaoSalvar.Location = new System.Drawing.Point(205, 238);
+            this.botaoSalvar.Name = "botaoSalvar";
+            this.botaoSalvar.Size = new System.Drawing.Size(75, 23);
+            this.botaoSalvar.TabIndex = 1;
+            this.botaoSalvar.Text = "Salvar";
+            this.botaoSalvar.UseVisualStyleBackColor = true;
+            this.botaoSalvar.Click += new System.EventHandler(this.botaoSalvar_Click);
+            // 
+            // Editor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.botaoSalvar);
+            this.Controls.Add(this.texto);
+            this.Name = "Editor";
+            this.Text = "Editor de Texto";
+            this.Load += new System.EventHandler(this.Editor_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox texto;
+        private System.Windows.Forms.Button botaoSalvar;
+    }
+}
+
---
+++ AluraCSharpArquivo/Editor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AluraCSharpArquivo
+{
+    public partial class Editor : Form
+    {
+        private string arquivo = "arquivo.txt";
+
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        private void Editor_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(arquivo))
+            {
+                using (FileStream entrada = new FileStream(arquivo, FileMode.Open))
+                using (StreamReader leitor = new StreamReader(entrada))
+                {
+                    texto.Text = leitor.ReadToEnd();
+                }
+            }
+        }
+
+        private void botaoSalvar_Click(object sender, EventArgs e)
+        {
+            using (FileStream saida = new FileStream(arquivo, FileMode.Create))
+            using (StreamWriter escritor = new StreamWriter(saida))
+            {
+                escritor.Write(texto.Text);
+            }
+        }
+    }
+}
